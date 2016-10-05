2d5d3373c548570350c683e1b48f33921d3e98ac
mittman <mittman@users.noreply.github.com>
2016-10-04 22:48:11 -0700

    Restructure into functions and file input parameters

--- validate.sh
+++ validate.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #################
 # validate.sh ###
-version="1.4" ###
+version="1.5" ###
 ### mittman #####
 #################
 
 # Input files
-html=("index.html")
-css=("style.css")
-js=("app.js" "client.js")
-json=("db.json")
+#html=("index.html")
+#css=("style.css")
+#js=("app.js" "client.js")
+#json=("db.json")
 
 # Tests to run
 list="jshint jsonlint csslint tidy whitespace stylish"
@@ -24,6 +24,40 @@ warn="\e[1;33m"
 fail="\e[1;31m"
 invert="\e[7m"
 reset="\e[0m"
+
+printusage() {
+  echo "USAGE: validate.sh <file> <file>"
+  echo '    --config       - alt config file (default: $PWD/.webapp)'
+  echo '    -v, --version  - print version'
+  echo '    -h, --help     - outputs this message'
+}
+
+printversion() {
+  echo "validate.sh -- version $version"
+  echo -e "\nUtility to easily run webapp validation tests"
+  echo "Copyright (c) 2016 mittman"
+  echo "Dual licensed: MIT (aka X11) and GPLv2"
+}
+
+sampleconfig() {
+  echo -e "\n${alert}==>${reset} ${notice}Creating $config file${reset}"
+  echo 'html=("index.html" "about.html")' > $config
+  echo 'css=("style.css")' >> $config
+  echo 'js=("js/app.js" "server.js")' >> $config
+  echo 'json=("db.json")' >> $config
+  echo 'list="whitespace stylish jshint jsonlint csslint tidy"' >> $config
+  cat $config 2>/dev/null
+}
+
+readmenodejs() {
+  echo -e "\n${alert}==> ${notice}NOTE: nodeJS users${reset}"
+  echo -e "  Add to ${fail}package.json${reset}"
+  echo -e "    ${invert}  ""\"scripts""\": {${reset}"
+  echo -e "    ${invert}    ""\"test""\": ""\"/path/to/validate.sh""\"${reset}"
+  echo -e "    ${invert}  },${reset}"
+  echo -e "${reset}  Then ${alert}\$${reset} ${warn}npm test${reset}\n"
+}
+
 
 run_utility() {
   if checkdep $cmd; then
@@ -44,6 +78,26 @@ run_utility() {
     done
   else
     echo -e "${warn}Missing ${cmd}${reset}. Skipping ${what} validation"
+  fi
+}
+
+checkfiles() {
+  exists="0"
+  notfound="0"
+
+  for file in $@; do
+    if [ -f "$file" ]; then
+      exists=$((exists+1))
+    else
+      notfound=$((notfound+1))
+    fi
+  done
+
+  if [ $exists -eq 0 ]; then
+    if [ $notfound -gt 0 ]; then
+      echo -e "${warn}No files found${reset}"
+      exit 1
+    fi
   fi
 }
 
@@ -118,43 +172,50 @@ parse_utils() {
 }
 
 
+# Parse parameters
 if [ ! -z "$1" ]; then
-  echo "validate.sh -- version $version"
-  echo -e "\nUtility to easily run webapp validation tests"
-  echo "Copyright (c) 2016 mittman"
-  echo "Dual licensed: MIT (aka X11) and GPLv2"
-  [ "$1" = "--version" ] && exit 0
-
+  printversion
+  while [[ "$1" ]]; do
+    if [ "$1" = "-v" -o "$1" = "--version" ]; then
+      exit 0
+    elif [ "$1" = "-h" -o "$1" = "--help" ]; then
+      printusage
+      exit 0
+    elif [ "$1" = "--config" ]; then
+      config="$2"
+      shift
+    elif [ -f "$1" ]; then
+      [[ "$1" = *.html ]] && html+=("$1")
+      [[ "$1" = *.css ]] && css+=("$1")
+      [[ "$1" = *.js ]] && js+=("$1")
+      [[ "$1" = *.json ]] && json+=("$1")
+      input=("${html[@]}" "${css[@]}" "${json[@]}" "${js[@]}")
+    else
+      printusage
+      exit 1
+    fi
+    shift
+  done
+  [ ! -z "$input" ] && unset config
+else
   if [ ! -f "$config" ]; then
-    echo -e "\n${alert}==>${reset} ${notice}Creating $config file${reset}"
-    echo 'html=("index.html" "about.html")' > $config
-    echo 'css=("style.css")' >> $config
-    echo 'js=("js/app.js" "server.js")' >> $config
-    echo 'json=("db.json")' >> $config
-    echo 'list="jshint jsonlint csslint tidy whitespace stylish"' >> $config
-    cat $config 2>/dev/null
+    sampleconfig
+    readmenodejs
+    exit 1
   fi
-
-  echo -e "\n${alert}==> ${notice}NOTE: nodeJS users${reset}"
-  echo -e "  Add to ${fail}package.json${reset}"
-  echo -e "    ${invert}  ""\"scripts""\": {${reset}"
-  echo -e "    ${invert}    ""\"test""\": ""\"/path/to/validate.sh""\"${reset}"
-  echo -e "    ${invert}  },${reset}"
-  echo -e "${reset}  Then ${alert}\$${reset} ${warn}npm test${reset}\n"
-
-  echo "USAGE: validate.sh [--version]"
-  exit 1
 fi
 
 # Load config file
 if [ -f "$config" ]; then
   echo -e "${alert}==>${reset} ${notice}Parsing ($config) config file${reset}"
   source "$config"
+  input=("${html[@]}" "${css[@]}" "${json[@]}" "${js[@]}")
 else
   echo -e "${warn}==>${reset} ${notice}Config file ($config) not found${reset}"
 fi
 
 # Run tests
+checkfiles "${input[@]}"
 parse_utils
 
 ### END ###
